d4f6046b2e7ec920b5d7889e0638c9af3eebb47f
Ivo Santiago <ivo@goodtime.io>
2019-01-03 15:48:51 -0200

    Day 01 - Classes 01, 02

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    <Link to="/">Reduxstagram</Link>
+                </h1>
+            </div>
+        );
+    }
+}
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/style.styl';
+
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
