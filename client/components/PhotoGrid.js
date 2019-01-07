eb3dcda79588ea812511217a0ed993ec396e4f27
Ivo Santiago <ivo@goodtime.io>
2019-01-07 15:44:13 -0200

    Day 03 - Classes 09, 10, 11, 12

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Photo from './Photo';
 
 export default class PhotoGrid extends React.Component {
     render() {
-        return <div className="photo-grid">I'm photo grid</div>;
+        return (
+            <div className="photo-grid">
+                {this.props.posts.map((post, i) => (
+                    <Photo {...this.props} key={i} i={i} post={post} />
+                ))}
+            </div>
+        );
     }
 }
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,17 @@
 function posts(state = [], action) {
-    console.log(state, action);
-    return state;
+    switch (action.type) {
+        case 'INCREMENT_LIKES':
+            return [
+                ...state.slice(0, action.index),
+                {
+                    ...state[action.index],
+                    likes: state[action.index].likes + 1
+                },
+                ...state.slice(action.index + 1)
+            ];
+        default:
+            return state;
+    }
 }
 
 export default posts;
